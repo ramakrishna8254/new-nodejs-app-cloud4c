8fa0c96c8d21f400720f9bc273d0ea4cc8aeb2fb
ramakrishna8254 <109083683+ramakrishna8254@users.noreply.github.com>
2023-03-15 19:33:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
+
+class HelloWorld {
+  constructor(options) {
+      this.options = options || {};
+      this._greeting = this.options.greeting || 'Hello World!';
+  }
+
+  get greeting() {
+    return this._greeting;
+  }
+
+  set greeting( message ) {
+    this._greeting = message || '';
+  }
+
+  greet() {
+    console.log(this.greeting);
+  }
+}
+
+// define factory function(s)
+
+function createHelloWorld(options) {
+  return new HelloWorld(options);
+}
+
+// export the factory functions
+
+module.exports = {
+  createHelloWorld
+}
+
 const express = require('express')
 const app = express()
 const port = 9981
